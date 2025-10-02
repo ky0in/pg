c15d3942a909508f713a0cb33b4f34c50fb15a10
ky0in <154435878+ky0in@users.noreply.github.com>
2025-10-02 12:15:40 +0000

    prvni cviceni

---
+++ cviceni1.py
@@ -0,0 +1,19 @@
+
+
+def cislo_mensi_nez_3(hodnota):
+     if hodnota > 3:
+          print(f'Hodnota {hodnota} je vetsi nez 3')
+     elif hodnota < 3:
+          print(f'Hodnota {hodnota} je mensi nez 3')
+     else
+          print(f'Hodnota {hodnota} je rovna nez 3')
+    
+
+if __name__ == "__main__":
+    
+     cislo = input("Zadej cislo: ")
+     cislo = int(cislo)
+     print(f"Zadane cislo je: {cislo}")
+
+     cislo_mensi_nez_3(1)
+     cislo_mensi_nez_3(cislo)
