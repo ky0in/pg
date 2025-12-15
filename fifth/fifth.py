c11e29bc3ccd585ea65a4be575ac59168994cd65
ky0in <154435878+ky0in@users.noreply.github.com>
2025-12-15 18:31:57 +0000

    fifth hw

--- fifth/fifth.py
+++ fifth/fifth.py
@@ -1,4 +1,3 @@
-
 import sys
 
 # definice úvodních binárních sekvencí obrázkových souborů
@@ -7,14 +6,16 @@ gif_header1 = b'GIF87a'
 gif_header2 = b'GIF89a'
 png_header = b'\x89PNG\r\n\x1a\n'
 
-# využít poznatky z prezentace - práce se soubory
+
 def read_header(file_name, header_length):
     """
     Tato funkce načte binární soubor z cesty file_name,
     z něj přečte prvních header_length bytů a ty vrátí pomocí return
     """
-    with open(file_name,"rb") as soubor:
-        return soubor.read(header_length)
+    with open(file_name, 'rb') as f:
+        header = f.read(header_length)
+    return header
+
 
 def is_jpeg(file_name):
     """
@@ -23,23 +24,35 @@ def is_jpeg(file_name):
     """
     # načti hlavičku souboru
     header = read_header(file_name, len(jpeg_header))
-    return header.startswith(jpeg_header)
+
+    # vyhodnoť zda je soubor jpeg
+    if header == jpeg_header:
+        return True
+    return False
 
 def is_gif(file_name):
     """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku gif,
+    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
     tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
     """
+    # vyhodnoť zda je soubor gif
     header = read_header(file_name, len(gif_header1))
-    return header.startswith(gif_header1) or header.startswith(gif_header2)
+    if header == gif_header1 or header == gif_header2:
+        return True
+    return False
+
 
 def is_png(file_name):
     """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku png,
+    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
     tu srovná s definovanou hlavičkou v proměnné png_header
     """
+    # vyhodnoť zda je soubor png
     header = read_header(file_name, len(png_header))
-    return header.startswith(png_header)
+    if header == png_header:
+        return True
+    return False
+
 
 def print_file_type(file_name):
     """
@@ -54,18 +67,14 @@ def print_file_type(file_name):
     else:
         print(f'Soubor {file_name} je neznámého typu')
 
+
 if __name__ == '__main__':
+    # přidej try-catch blok, odchyť obecnou vyjímku Exception a vypiš ji
     try:
-        # Zkontrolujte, zda byl zadán název souboru
-        if len(sys.argv) < 2:
-            raise ValueError("Musíte zadat název souboru jako argument.")
+        # získej jméno souboru ze vstupních argumentů
+        file_name = sys.argv[1]
 
-        file_name = sys.argv[1]  # hledá parametr vedle fifth.py
+        # zavolej funkci pro vypsání typu souboru
         print_file_type(file_name)
-
-    except FileNotFoundError:
-        print(f'Chyba: Soubor {file_name} nebyl nalezen.')
-    except ValueError as ve:
-        print(f'Chyba: {ve}')
     except Exception as e:
-        print(f'Nastala neočekávaná chyba: {e}')
+        print(f'Chyba: {e}')
